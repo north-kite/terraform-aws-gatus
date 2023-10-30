3bb55abbab4838141573dd1967db4c0256e622ac
Daniel Hill <dan@mamu.co>
2023-10-30 10:16:17 +0000

    feat(examples): add building image with config to examples

--- examples/default/provider.tf
+++ examples/default/provider.tf
@@ -1,7 +1,23 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
+}
+
 provider "aws" {
   default_tags {
     tags = {
       Name = "${var.env}-gatus-default"
     }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+  registry_auth {
+    address = "registry.hub.docker.com"
   }
 }
