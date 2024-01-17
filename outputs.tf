784d57e5ccf810af687f7d4a3bd70a706ba87c68
Daniel Hill <dan@mamu.co>
2024-01-17 17:26:38 +0000

    feat: output DNS name of load balancer
    This DNS name can be used directly or in a custom DNS record.

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "security_group_id" {
   description = "Security group assigned to Gatus container. Add rules here to grant Gatus access to endpoint to monitor."
   value       = aws_security_group.gatus.id
 }
+
+output "alb_dns_name" {
+  value = var.alb == null ? aws_lb.alb[0].dns_name : null
+}
